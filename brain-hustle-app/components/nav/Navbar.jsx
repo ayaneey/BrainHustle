1abfd5c5fc1fff7786d5f21c80fa74d3062ce1ac
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2023-09-05 14:42:18 +0100

    making adjustment to navbar on all breakpoints

--- brain-hustle-app/components/nav/Navbar.jsx
+++ brain-hustle-app/components/nav/Navbar.jsx
@@ -1,55 +1,33 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
 
 function Navbar() {
+	const [isSmallOrMediumScreen, setIsSmallOrMediumScreen] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsSmallOrMediumScreen(window.innerWidth <= 1023); // Adjust breakpoint as needed
+		};
+
+		// Listen for window resize events
+		window.addEventListener("resize", handleResize);
+
+		// Initial check on component mount
+		handleResize();
+
+		// Remove event listener on component unmount
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<nav className="bg-gray-800">
 			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 				<div className="relative flex h-16 items-center justify-between">
 					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-						{/*  Mobile menu button--> */}
-						<button
-							type="button"
-							className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-							aria-controls="mobile-menu"
-							aria-expanded="false"
-						>
-							<span className="absolute -inset-0.5"></span>
-							<span className="sr-only">Open main menu</span>
-
-							{/*  Icon when menu is closed. */}
-
-							<svg
-								className="block h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-								/>
-							</svg>
-
-							{/*  Icon when menu is open. */}
-
-							<svg
-								className="hidden h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M6 18L18 6M6 6l12 12"
-								/>
-							</svg>
-						</button>
+						{/* ... (Mobile menu button) */}
 					</div>
 					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 						<div className="flex flex-shrink-0 items-center">
@@ -59,51 +37,83 @@ function Navbar() {
 								alt="Your Company"
 							/>
 						</div>
-						<div className="hidden sm:ml-6 sm:block md:ml-60 lg:ml-72">
+						<div className="hidden sm:ml-32 sm:block md:ml-44 lg:ml-72">
 							<div className="flex space-x-4">
-								<a
-									href="/"
-									className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-									aria-current="page"
-								>
-									Home
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-								>
-									Product
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-								>
-									FAQ
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-								>
-									Contact
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-								>
-									My Account
-								</a>
+								{isSmallOrMediumScreen ? (
+									// Small and Medium Screens
+									<>
+										<a
+											href="/"
+											className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+											aria-current="page"
+										>
+											Home
+										</a>
+										<a
+											href="#"
+											className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+										>
+											Product
+										</a>
+										<a
+											href="#"
+											className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+										>
+											Contact
+										</a>
+										<button
+											type="button"
+											className="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium"
+										>
+											Login
+										</button>
+									</>
+								) : (
+									// Large and Extra Large Screens
+									<>
+										<a
+											href="/"
+											className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+											aria-current="page"
+										>
+											Home
+										</a>
+										<a
+											href="#"
+											className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+										>
+											Product
+										</a>
+										<a
+											href="#"
+											className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+										>
+											Contact
+										</a>
+										<a
+											href="#"
+											className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+										>
+											My Account
+										</a>
+										<button
+											type="button"
+											className="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium"
+										>
+											Login
+										</button>
+										<button
+											type="button"
+											className="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium"
+										>
+											Become a member
+										</button>
+									</>
+								)}
 							</div>
 						</div>
 					</div>
-					<div className="hidden sm:block">
-						<button
-							type="button"
-							className="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium"
-						>
-							Login
-						</button>
-					</div>
-					<div className="hidden sm:block md:ml-8 lg:ml-10">
+					<div className="sm:hidden">
 						<button
 							type="button"
 							className="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium"
@@ -111,15 +121,11 @@ function Navbar() {
 							Become a member
 						</button>
 					</div>
-
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"></div>
 				</div>
 			</div>
-
-			{/* {Mobile menu, show/hide based on menu state. */}
 			<div className="sm:hidden" id="mobile-menu">
 				<div className="space-y-1 px-2 pb-3 pt-2">
-					{/*Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white*/}
 					<a
 						href="/"
 						className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
@@ -132,12 +138,6 @@ function Navbar() {
 						className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
 					>
 						Product
-					</a>
-					<a
-						href="#"
-						className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-					>
-						FAQ
 					</a>
 					<a
 						href="#"
