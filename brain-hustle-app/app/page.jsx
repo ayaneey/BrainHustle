86f749c81c6f5772181947a0185767411843a167
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2023-09-10 21:03:41 +0100

    worked on making the sm-tablet responsive

--- brain-hustle-app/app/page.jsx
+++ brain-hustle-app/app/page.jsx
@@ -3,7 +3,7 @@ import Homepage from "../components/Homepage";
 
 function Home() {
 	return (
-		<div className="mt-4 flex flex-col lg:flex-row xl:ml-8">
+		<div className="mt-4 flex flex-col lg:flex-row xl:ml-8 ">
 			<div className="lg:w-1/2">
 				<h1 className="custom-h1 text-2xl sm:text-5xl sm:ml-5 md:text-4xl lg:text-4xl xl:text-7xl sm-phone:text-4xl sm-phone:text-center md-phone:text-5xl sm-tablet:text-5xl font-semibold font-raleway-700 text-base-black leading-[1.5] xl:leading-[1.2] tracking-[3.78px] max-w-[674px] xl:mt-12 xl:flex xl:flex-col ">
 					<span className="xl:mb-4">Hustle Hard, </span>
@@ -16,11 +16,11 @@ function Home() {
 					a quam consectetur porro asperiores aspernatur. Accusantium excepturi
 					ducimus vel eius aliquam.
 				</p>
-				<button className="custom-button mt-4 sm:mt-20 md:mt-28 lg:mt-6 xl:mt-12 xl:text-white xl:text-center xl:font-raleway xl:text-30 xl:font-normal xl:leading-120 xl:tracking-wide">
+				<button className="custom-button mt-4 sm:mt-20 md:mt-28 lg:mt-6 xl:mt-12 xl:text-white xl:text-center xl:font-raleway xl:text-30 xl:font-normal xl:leading-120 xl:tracking-wide  sm-tablet:ml-96 ">
 					Get In Touch
 				</button>
 			</div>
-			<div className="lg:w-1/2 md:w-full">
+			<div className="lg:w-1/2 ">
 				{" "}
 				{/* Adjust width for md */}
 				<Homepage />
--- brain-hustle-app/components/Homepage.jsx
+++ brain-hustle-app/components/Homepage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Homepage() {
 	return (
-		<div className="sm:w-full max-w-xs sm:max-w-sm  lg:max-w-lg xl:max-w-xl mx-auto custom-image-class sm-phone:max-w-xs md-phone:max-w-xs lg-phone:max-w-xs xl-phone:max-w-xs sm-tablet:max-w-sm lg-tablet:max-w-sm xl-tablet:max-lg sm:order-first md:max-h-60 md:mr-8  ">
-			<img src="./images/pupil.svg" alt="Home" className="w-full  " />
+		<div className="sm:w-full max-w-xs sm:max-w-sm  lg:max-w-lg xl:max-w-xl mx-auto custom-image-class sm-phone:max-w-xs md-phone:max-w-xs lg-phone:max-w-xs xl-phone:max-w-xs sm-tablet:max-w-xs lg-tablet:max-w-sm xl-tablet:max-lg sm:order-first md:max-h-60 md:mr-8 sm-tablet:ml-5">
+			<img src="./images/pupil.svg" alt="Home" className="w-full" />
 		</div>
 	);
 }
