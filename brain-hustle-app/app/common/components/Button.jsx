2d30658627806fde9dc269a5f6688d9aa36cc018
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2023-11-13 20:47:53 +0000

    centered the button on Hero for all breakpoints

--- brain-hustle-app/app/common/components/Button.jsx
+++ brain-hustle-app/app/common/components/Button.jsx
@@ -1,64 +1,28 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function Button({ title, smallOnMobile, mediumOnMobile }) {
-	const buttonStyle = {
+	let buttonStyle = {
 		width: "200px",
-		borderRadius: "8px",
+		borderRadius: "5px",
 		gap: "10px",
-		padding: "12px 30px",
+		padding: "16px 40px",
 	};
 
-	if (smallOnMobile) {
-		// Styling smaller buttons on smaller screens
-		buttonStyle.width = "130px";
-		buttonStyle.padding = "6px 8px";
+	if (smallOnMobile || mediumOnMobile) {
+		buttonStyle.width = "150px";
+		buttonStyle.padding = "12px 20px";
 	}
-
-	if (mediumOnMobile) {
-		// Styling smaller buttons on medium screens
-		buttonStyle.width = "130px";
-		buttonStyle.padding = "6px 8px";
-	}
-
-	const [marginLeft, setMarginLeft] = useState("70px");
-
-	useEffect(() => {
-		const handleResize = () => {
-			const screenWidth = window.innerWidth;
-
-			if (screenWidth >= 320 && screenWidth < 350) {
-				setMarginLeft("70px");
-			} else if (screenWidth >= 350 && screenWidth <= 444) {
-				setMarginLeft("100px");
-			} else if (screenWidth >= 445 && screenWidth <= 600) {
-				setMarginLeft("90px");
-			} else {
-				setMarginLeft("70px"); // Default marginLeft
-			}
-		};
-
-		// Add event listener for window resize
-		window.addEventListener("resize", handleResize);
-
-		// Initial call to set the margin based on the current window width
-		handleResize();
-
-		// Remove the event listener when the component unmounts
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
 
 	return (
-		<div
-			className="bg-box text-primaryColor hover.bg-gray-700 hover:text-white rounded-md p-2 inline-flex items-center"
-			style={{ marginLeft, textAlign: "center" }}
-		>
-			<button className="p-2" style={buttonStyle}>
-				{title} Get in Touch
-			</button>
+		<div className="flex justify-center">
+			<div className="text-center">
+				<button
+					className="bg-box text-primaryColor hover:bg-gray-700 hover:text-white rounded-md p-2 inline-flex items-center"
+					style={buttonStyle}
+				>
+					<span className="mx-auto">{title} Get in Touch</span>
+				</button>
+			</div>
 		</div>
 	);
 }
--- brain-hustle-app/app/home/Hero.jsx
+++ brain-hustle-app/app/home/Hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
 	const title = "";
 	return (
 		<div>
-			<div className="w-full flex md:flex-row flex-col">
-				<div className="md:w-1/2 p-5">
-					<h1 className="custom-h1 sm-phone:text-[35px] sm-phone:text-center md:text-[45px] sm:text-[55px] sm:text-center lg-phone:text-[40px] sm-tablet:text-[45px]  font-semibold font-raleway-700 text-base-black leading-[1.5] tracking-[3.78px] max-w-[674px]  flex  flex-col ">
+			<div className="w-full flex md:flex-row flex-col p-5">
+				<div className="md:w-1/2 ">
+					<h1 className="custom-h1 sm-phone:text-[35px] sm-phone:text-center md:text-[45px] sm:text-[55px] sm:text-center lg-phone:text-[40px] sm-tablet:text-[45px]  font-semibold font-raleway-700 text-base-black leading-[1.5] tracking-[3.78px] max-w-[674px]  flex  flex-col  ">
 						<span className="">Hustle Hard, </span>
 						<span>Excel Harder!</span>
 					</h1>
