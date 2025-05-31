93ecbace5446fa19ddc843c132495d3d31474a35
Ayan Ahmed <ayanahmed@MacBookPro.attlocal.net>
2025-05-31 13:14:13 -0400

    renamed file

--- brain-hustle-app/libs/prismadb.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-
-// Declare global variable
-global.prisma = global.prisma || new PrismaClient();
-
-// Check if not in production environment
-if (process.env.NODE_ENV !== "production") global.prisma = global.prisma;
-
-module.exports = global.prisma;
